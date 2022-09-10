fc95d8c7b8551e3687eab5d10821ba74c71c034e
Waldemir Rodrigues <waldemir.rodrigues88@gmail.com>
2022-09-10 16:48:20 -0300

    first commit

---
+++ calculo.js
@@ -0,0 +1,12 @@
+var n1 = document.querySelector('#n1')
+var n2 = document.querySelector('#n2')
+var n3 = document.querySelector('#n3')
+var n4 = document.querySelector('#n4')
+var resultado = document.querySelector('span')
+
+function somar() {
+  resultado.innerHTML = (parseFloat(n1.value) + parseFloat(n2.value) + parseFloat(n3.value) + parseFloat(n4.value)) /4
+}
+
+// Revisão
+//Variaveis, strings, consolo.log, to.fixed, operações matemática, concatenação, console para ver os resultaodos
